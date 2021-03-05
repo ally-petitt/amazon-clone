7d7bef28cc52782574a6cf0ea0532e274111b383
ally-petitt <allypetitt@gmail.com>
2021-03-05 13:44:26 -0800

    completed navbar

---
+++ src/Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAikG8YKnv6Otwono1CfzSbe8YgWDw-33U",
+    authDomain: "clone-a30c4.firebaseapp.com",
+    projectId: "clone-a30c4",
+    storageBucket: "clone-a30c4.appspot.com",
+    messagingSenderId: "180773774287",
+    appId: "1:180773774287:web:fb4783d54edbb9daf768ba",
+    measurementId: "G-YF62SN6R6Q"
+  };
